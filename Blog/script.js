0c92c8908691369c0b28f193a9f7bb9aad891d98
LuisDann <167117174+LuisDann@users.noreply.github.com>
2025-01-17 15:32:03 -0300

    agora vai

--- Blog/script.js
+++ Blog/script.js
@@ -1,36 +1,9 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function openModal(index) {
-    document.getElementById("modal").style.display = "block";
-    slideIndex = index;
-    showSlides(slideIndex);
-    
-    // Desabilitar a rolagem da página
-    document.body.style.overflow = "hidden";
-}
-
-function closeModal() {
-    document.getElementById("modal").style.display = "none";
-    
-    // Reabilitar a rolagem da página
-    document.body.style.overflow = "auto";
-}
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-    let slides = document.getElementsByClassName("modal-slide");
-    if (n > slides.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = slides.length; }
-
-    // Esconde todas as imagens
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    // Mostra a imagem atual
-    slides[slideIndex - 1].style.display = "block";
-}
+// JavaScript for Parallax Effect
+window.addEventListener('scroll', function () {
+    const parallax = document.querySelectorAll('.parallax');
+    parallax.forEach(function (element) {
+        const speed = 0.5;
+        const offset = window.pageYOffset * speed;
+        element.style.backgroundPosition = `center ${offset}px`;
+    });
+});
